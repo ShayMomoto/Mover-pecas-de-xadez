b90d26a40d2ae30250272d00acefd94b55883337
João Santos <153041574+ShayMomoto@users.noreply.github.com>
2025-04-03 20:27:38 +0000

    Nivel Aventureiro

--- Nivel-Aventureiro.c
+++ Nivel-Aventureiro.c
@@ -1,53 +1,21 @@
 #include <stdio.h>
 
-int main()
-{
-//Usando FOR para mover a torre para a direita 5x
-printf("--- Movendo a Torre ---\n");
-for (int i = 1; i <= 5; i++)
-{
-    printf(" Direita\n");
-   
-}
-printf("\n");
-printf("\n");
+int main() {
+    int i, j;
     
-//Mover Bispo 5 casas na diagonal para cima e à direita (Usando while)
-    printf("=== Movendo o bispo ===\n");
-    int diagonal = 1;
-    while (diagonal <= 5)
-    {
-        printf("Cima, Direita\n");
-        diagonal++;
-        
+    printf("~~~Movimentação do Cavalo~~~\n");
+    
+    
+    for (i = 0; i < 2; i++) {
+        printf("Baixo\n");
     }
-    printf("\n");
-    printf("\n");
-        
-//Movendo a rainha 8 casas para a esquerda (usando DO-While)
-        printf("*** Movendo a Rainha ***\n");
-        int rainha = 1;
-        do
-        {
+    
+    
+    for (i = 0; i < 1; i++) {
+        for (j = 0; j < 1; j++) {
             printf("Esquerda\n");
-            rainha++;
-        } while (rainha <= 8);
-
-//Movendo cavalo 
-            printf("~~~ Movendo o Cavalo ~~~");
-            int cavaloBaixo;
-
-            for (cavaloBaixo = 0; cavaloBaixo <= 2; cavaloBaixo++)
-            {
-                printf("Baixo\n");
-                int cavaloEsquerda;
-                for (cavaloEsquerda = 0; cavaloEsquerda <= 1; cavaloEsquerda++);
-                {
-                    printf("esquerda\n");
-                }
-            }
-
-        return 0;
-        
-
+        }
+    }
+    
+    return 0;
 }
