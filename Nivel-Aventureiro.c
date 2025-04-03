a578b4d8e95d249a3e21d33376f23dd5b20453fc
João Santos <153041574+ShayMomoto@users.noreply.github.com>
2025-04-03 20:31:38 +0000

    Nivel Aventureiro

--- Nivel-Aventureiro.c
+++ Nivel-Aventureiro.c
@@ -10,12 +10,10 @@ int main() {
         printf("Baixo\n");
     }
     
-    
-    for (i = 0; i < 1; i++) {
         for (j = 0; j < 1; j++) {
             printf("Esquerda\n");
         }
-    }
+    
     
     return 0;
 }
